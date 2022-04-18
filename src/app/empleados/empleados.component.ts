08b3f27cb1bf5f3725989b23524316abf3cb58b8
Leal31 <lealemanuel31@protonmail.com>
2022-04-18 09:39:53 -0500

    commit

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleados.component.css']
 })
 export class EmpleadosComponent implements OnInit {
+  
+  Nombre = "Juan";
+
+  Apellido = "Diaz";
+  
+  Edad = 18;
+
+  textoRegistro = "No hay nadie registrado";
+
+  Empresa = "Google";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  HabilitacionCuadro = true;
+
+  usuarioRegistrado = true;
+  
+  getRegistroUsuario() {
+    this.usuarioRegistrado = false;
+  }
+
+  setUsuarioRegistrado(event:Event) {
+
+    if ((<HTMLInputElement>event.target).value == "si") {
+      this.textoRegistro = "El usuario se acaba de registrar";
+    } else {
+      this.textoRegistro = "No hay nadie registrado";
+    }
+  }
 }
